beae378ad07297eab6efb7764c60e0dcb5a83685
ALFaqih12 <alfaqiha396@gmail.com>
2026-07-04 15:44:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const billData = {
+  pln: {
+    "123456789012": { name: "Budi Santoso", address: "Jl. Merdeka No. 123, Jakarta", period: "Jan-Feb 2025", amount: 245000, fine: 0, dueDate: "2025-02-15" },
+    // more...
+  },
+  pdam: { /* similar */ },
+  internet: { /* similar */ },
+  seminar: { /* similar */ },
+  spp: {
+    "202310001": [
+      { id: 1, desc: "SPP Semester Ganjil 2025/2026 - Cicilan ke-1", amount: 2500000, status: "unpaid" },
+      // more...
+    ]
+  }
+};
